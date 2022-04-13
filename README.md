c4493868739c36eb9ec3553a65ca4d71af8960b9
Jea-Eok-Kim <je.kim@xiilab.com>
2022-04-13 09:57:41 +0900

    :lipstick: 로그인 테마 변

--- README.md
+++ README.md
@@ -1,46 +1,50 @@
-# Getting Started with Create React App
+<h1 align="center">Uyuni keycloak Theme 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
+</p>
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Install
 
-## Available Scripts
+```sh
+npm install
+```
 
-In the project directory, you can run:
+## Usage
 
-### `npm start`
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+1. dev 서버 시작하기
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+    ```sh
+    npm run start
+    ```
 
-### `npm test`
+2. `src/utils/keycloakManager.ts` 파일에서 27번째 주석 헤재
+    ```typescript
+      ({
+      // @cindy.choi
+      // 아래의 디버깅을 원하는 페이지 주석을 해제하면 하단의 mockData를 적용하여 로컬에 페이지를 띄울 수 있습니다.
+       "mockPageId": "login.ftl",
+      // "mockPageId": "register.ftl",
+      // "mockPageId": "login-update-profile.ftl",
+      // // @ts-ignore
+      // "mockPageId": "login-update-password.ftl",
+      // "mockPageId": "info.ftl",
+      // "mockPageId": "error.ftl",
+    ```
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Build and deploy
 
-### `npm run build`
+1. 다음 명령어 실행
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+    ```sh
+    npm run keycloak
+    ```
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+2. `build_keycloak/src/main/resources/theme` 폴더안에 있는 폴더들을 keycloak 설치 서버의 `/opt/jboss/keycloak/themes` 으로 복사
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## Show your support
 
-### `npm run eject`
+Give a ⭐️ if this project helped you!
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- src/pages/keycloak/Login.tsx
+++ src/pages/keycloak/Login.tsx
@@ -6,42 +6,142 @@ import { Button, TextField } from '@mui/material';
 import type { KcProps } from 'keycloakify/lib/components/KcProps';
 import type { KcContextType } from '@/utils/keycloakManager';
 
-import bg from '@/assets/images/login-bg.jpg';
+import CssBaseline from "@mui/material/CssBaseline";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Paper from "@mui/material/Paper";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+
+import back_ground_image from "@/assets/images/login-image.png";
+import back_logo_image from "@/assets/images/login-logo.svg";
+import back_text_image from "@/assets/images/login-text.png";
 
 type KcContext_Login = Extract<KcContextType, { pageId: 'login.ftl' }>;
 
-const StyledLogin = styled.div`
-  min-width: 100vw;
-  min-height: 100vh;
-  background-image: url(${bg});
-  background-size: cover;
-  background-repeat: no-repeat;
+
+// login
+
+export const LoginText = styled(Typography)`
+  font-family: Roboto;
+  font-size: 40px;
+  font-weight: 300;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: normal;
+  color: #04abfc;
+`;
+
+export const LeftContainer = styled(Grid)`
+  display: flex;
+  flex-direction: column !important;
+  background-color: #2184ff;
+  justify-content: flex-start;
+  @media screen and (max-width: 599px) {
+    display: none;
+  }
+  @media screen and (max-width: 899px) {
+    display: none;
+  }
+`;
+
+export const RightContainer = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const LoginBackGroundImage = styled.img`
+  margin-top: 124.5px;
+  width: 335.8px;
+  height: 241px;
+`;
+export const LoginBackGroundBox = styled(Box)`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  height: 80%;
 `;
+
+export const LoginBackTextImage = styled.img`
+  width: 628px;
+  height: 117px;
+  text-align: center;
+`;
+
+export const LoginBackLogoImage = styled.img`
+  width: 149.2px;
+  height: 24.6px;
+`;
+export const LoginBackLogoBox = styled(Box)`
+  padding-top: 44px;
+  padding-left: 56px;
+  padding-right: 56px;
+  height: 10%;
+`;
+
+export const LoginBackFooterBox = styled(Box)`
+  height: 10%;
+  display: flex;
+  align-items: flex-end;
+`;
+export const LoginFooter = styled(Box)`
+  display: flex;
+  width: 100%;
+  height: 52px;
+  justify-content: flex-start;
+  align-items: center;
+  padding-left: 52px;
+  background-image: linear-gradient(to right, #136fe4, #04abfc);
+`;
+
+export const CopyrightText = styled.p`
+  color: #ffffff;
+  margin: 0px;
+`;
+
+export const Submit = styled(Button)`
+  height: 68px;
+  box-shadow: 0 4px 8px 0 #b4d7ff;
+  background-color: #04abfc;
+  border-radius: 50px;
+`;
+
+// login
+
+
 
 const LoginForm = styled.form`
   width: 25rem;
   height: 15rem;
   background-color: white;
-  border-radius: 5px;
-  box-shadow: 2px 2px 8px 0px;
+  /* border-radius: 5px; */
+  /* box-shadow: 2px 2px 8px 0px; */
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
 
-const LoginInput = styled(TextField)`
-  width: 20rem;
-  margin-bottom: 8px !important;
-`;
 
-const LoginButton = styled(Button)`
-  width: 20rem;
-`;
+
+function Copyright(props: any) {
+  return (
+    <CopyrightText variant="body2" {...props}>
+      {"Copyright © "}
+      <Link color="inherit" href="https://xiilab.com/main">
+        system Corp.
+      </Link>{" "}
+      All Rights Reserved.
+    </CopyrightText>
+  );
+}
 
 export const Login = memo(
 	({ kcContext, ...props }: { kcContext: KcContext_Login } & KcProps) => {
@@ -68,24 +168,113 @@ export const Login = memo(
     }, []);
 
 		return (
-      <StyledLogin>
-        <LoginForm ref={form} method="post" action={url.loginAction}>
+      <Grid container component="main" sx={{ height: "100vh" }}>
+        <LeftContainer
+          item
+          xs={false}
+          sm={false}
+          md={9}
+          sx={{}}
+          data-class="LeftContainer"
+        >
+          <LoginBackLogoBox data-class="LoginBackLogoBox">
+            <LoginBackLogoImage
+              src={back_logo_image}
+              data-class="LoginBackLogoImage"
+            />
+          </LoginBackLogoBox>
+          <LoginBackGroundBox data-class="LoginBackGroundBox">
+            <LoginBackTextImage
+              src={back_text_image}
+              data-class="LoginBackTextImage"
+            />
+            <LoginBackGroundImage
+              src={back_ground_image}
+              data-class="LoginBackGroundImage"
+            />
+          </LoginBackGroundBox>
+          <LoginBackFooterBox data-class="LoginBackFooterBox">
+            <LoginFooter data-class="LoginFooter">
+              <Copyright sx={{ mt: 5 }} data-class="Copyright" />
+            </LoginFooter>
+          </LoginBackFooterBox>
+        </LeftContainer>
+
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={3}
+          component={Paper}
+          elevation={6}
+          square
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <RightContainer
+            sx={{
+              my: 8,
+              mx: 4,
+            }}
+          >
+            <LoginText variant="h5">Login</LoginText>
+            <LoginForm ref={form} method="post" action={url.loginAction}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                label={t("id")}
+                id="username"
+                name="username"
+                autoComplete="username"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                type="password"
+                label={t("password")}
+                id="password"
+                name="password"
+                autoComplete="current-password"
+              />
+              
+              <Submit
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                {t("login")}
+              </Submit>
+              
+            </LoginForm>
+          </RightContainer>
+        </Grid>
+
+        {/* <LoginForm ref={form} method="post" action={url.loginAction}>
           <LoginInput
             id="username"
             name="username"
             size="small"
-            label={t('id')}
+            label={t("id")}
           />
           <LoginInput
-            label={t('password')}
+            label={t("password")}
             id="password"
             name="password"
             type="password"
             size="small"
           />
-          <LoginButton variant="contained" onClick={() => handleSubmit()}>{ t('login') }</LoginButton>
-        </LoginForm>
-      </StyledLogin>
+          <LoginButton variant="contained" onClick={() => handleSubmit()}>
+            {t("login")}
+          </LoginButton>
+        </LoginForm> */}
+      </Grid>
     );
 	},
 );
